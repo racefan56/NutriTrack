b6b213f21dd1fc852feccf8838fc30bed4ddc3f5
Nicholas Smith <hamtorokaton@hotmail.com>
2022-03-08 17:07:17 -0600

    removed console log

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -8,8 +9,6 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-
-console.log(DB);
 
 mongoose
   .connect(DB, {
