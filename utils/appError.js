28f22165e47161183294156f7dacb1f505625a6d
Nicholas Smith <hamtorokaton@hotmail.com>
2022-07-11 17:51:55 -0500

    Attempting to get custom backend error messages to be recieved on the frontend

--- utils/appError.js
+++ utils/appError.js
@@ -1,11 +1,13 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     //SUPER is the parent class (Error in this case)
+    console.log(message);
     super(message);
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
+    this.data = message;
 
     Error.captureStackTrace(this, this.constructor);
   }
